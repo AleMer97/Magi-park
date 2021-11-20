8c3125ad98f324a76a908d3093c14b90c9a90b6a
Lukas Holzner <holzner@fs.tum.de>
2021-11-20 23:54:46 +0100

    Added Caching

--- Backend_Python/restapi.py
+++ Backend_Python/restapi.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
+from geopy import distance
+from datetime import datetime as dt
 import json
 
 app = Flask(__name__)
@@ -12,7 +14,8 @@ def get_parkingsspots(requ):
     matching = []
     for spot in parkingsspots:
         if spot['length'] >= float(requ["length"]):
-            if ( float(requ["swlat"]) <= spot['latitude'] <= float(requ["nelat"]) ) and ( float(requ["swlon"]) <=  spot['longitude'] <= float(requ["nelon"])):
+            spotloc = (float(requ["lat"]), float(requ["lon"]))
+            if distance.distance(spotloc, (spot['latitude'], spot['longitude'])).km <= float(requ["radius"]):
                 matching.append(spot)
     return matching
 
@@ -24,15 +27,17 @@ class GetParkingSpot(Resource):
     def get(self):
         requ = request.args.to_dict()
         print(requ)
-        if all(key in requ for key in ["swlat", "swlon", "nelat", "nelon", "length"]):
+        if all(key in requ for key in ["lat", "lon", "radius" ,"length"]):
             print("test")
             return get_parkingsspots(requ)
         else:
-            return "Error Missing Arguments", 400
+            return "Error Missing Arguments lat, lon, radius, length " , 400
 
 class AddParkingSpot(Resource):
     def post(self):
-        parkingsspots.append(json.loads(request.form['data']))
+        spot = json.loads(request.form['data'])
+        spot["timestamp"] = dt.now().isoformat(timespec="seconds")
+        parkingsspots.append(spot)
         print(str(parkingsspots))
         return
 
