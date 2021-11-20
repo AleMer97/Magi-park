8cf5602f2e5f9ce3541004c43a4d680e22b3a31f
Alexandre Mercier <alex.mercier@tum.de>
2021-11-20 17:06:37 +0100

    erste Karte wird angezeigt

--- Frontend/magipark/src/App.js
+++ Frontend/magipark/src/App.js
@@ -1,14 +1,45 @@
-import './App.css';
+import './index.css';
+import React, {useState} from "react";
+
+import CarLengthSlider from "./components/lengthSlider";
+import ChooseMapButton from "./components/MapChooseButton";
+
+import ShortTermMap from "./components/ShortTermMap";
+import LongTermMap from './components/LongTermMap';
+
+import { geolocated } from "react-geolocated";
 
 function App() {
+  const [carLength, setCarLength] = useState(6);
+
+  const [showLongTerm, setShowLongTerm] = useState(false);
+  const handleLongTerm = (event) => {
+    setShowLongTerm(event.target.checked)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Magi-park
-      </header>
-      <h2>
-        Some subtitle
-      </h2>
+    <div  class="grid place-items-center h-1/3">
+      
+        <h2>
+          Magi-park
+        </h2>
+        <p>
+          Find a parking spot. The easy way.
+        </p>
+        
+        <CarLengthSlider setCarLength={setCarLength}/>
+        <p>The length is: {carLength}</p>
+        
+        <ChooseMapButton showLongTerm={showLongTerm} handleLongTerm={handleLongTerm}/>
+        <p>Which map: {showLongTerm?"Long term":"Short term"}</p>
+      
+        <p>{geolocated.isGeolocationAvailable}</p>
+        
+
+        {showLongTerm? <LongTermMap/> : <ShortTermMap/>}
+
+        
+
     </div>
   );
 }
---
+++ Frontend/magipark/src/components/MapChooseButton.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+export default function ChooseMapButton({showLongTerm, handleLongTerm}) {
+    
+    return(
+        // Choose map
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch checked={showLongTerm} onChange={handleLongTerm} name="longtermswitch" />
+            }
+            label="Show Long-term map"
+          />
+        </FormGroup>
+    )
+}
